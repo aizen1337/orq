1d32d6d9175d84bfe6e0c15934b27f134a4c082c
AizeN1337 <maciejkalata79@gmail.com>
2025-02-02 21:21:06 +0100

    changes among auth apps

--- apps/auth/src/lib/auth.ts
+++ apps/auth/src/lib/auth.ts
@@ -1,9 +1,21 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { admin, organization } from "better-auth/plugins";
  
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: 'postgresql'
-    })
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
+    rateLimit: {
+        window:10,
+        max: 100
+    },
+    plugins: [
+        organization(),
+        admin()
+    ]
 })
